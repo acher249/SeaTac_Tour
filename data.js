ac384420ad1f9553a09757612e88d77d72666f63
acher249 <32597382+acher249@users.noreply.github.com>
2018-07-18 11:16:34 -0400

    udpate

--- data.js
+++ data.js
@@ -1,103 +1,7 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-seating-area",
-      "name": "Seating Area",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        },
-        {
-          "tileSize": 512,
-          "size": 4096
-        }
-      ],
-      "faceSize": 3200,
-      "initialViewParameters": {
-        "yaw": 3.115018901405506,
-        "pitch": -0.034363515617926055,
-        "fov": 1.5671975481182459
-      },
-      "linkHotspots": [
-        {
-          "yaw": -3.0442074123539715,
-          "pitch": -0.06258099004532625,
-          "rotation": 0,
-          "target": "1-vendor-area"
-        },
-        {
-          "yaw": 3.0408937863860857,
-          "pitch": -0.05573802821917617,
-          "rotation": 0,
-          "target": "2-bridge-to-concourse"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "1-vendor-area",
-      "name": "Vendor Area",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        },
-        {
-          "tileSize": 512,
-          "size": 4096
-        }
-      ],
-      "faceSize": 3200,
-      "initialViewParameters": {
-        "yaw": -2.45095933496232,
-        "pitch": 0.1301478932693403,
-        "fov": 1.5671975481182459
-      },
-      "linkHotspots": [
-        {
-          "yaw": 3.126432129861781,
-          "pitch": 0.08646648863167883,
-          "rotation": 0,
-          "target": "0-seating-area"
-        },
-        {
-          "yaw": -0.5748212255611538,
-          "pitch": 0.019447818034839415,
-          "rotation": 0,
-          "target": "2-bridge-to-concourse"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "2-bridge-to-concourse",
+      "id": "0-bridge-to-concourse",
       "name": "Bridge To Concourse",
       "levels": [
         {
@@ -124,22 +28,118 @@ var APP_DATA = {
       ],
       "faceSize": 3200,
       "initialViewParameters": {
-        "yaw": 3.0116714104408526,
-        "pitch": 0.0024339556005372742,
-        "fov": 1.5671975481182459
+        "yaw": 2.938646840120133,
+        "pitch": 0.02926348004838175,
+        "fov": 1.5707963267948966
       },
       "linkHotspots": [
         {
-          "yaw": -3.0096786270601648,
-          "pitch": 0.06926485154916939,
+          "yaw": -3.013774833666421,
+          "pitch": 0.056969612686277316,
           "rotation": 0,
-          "target": "0-seating-area"
+          "target": "1-seating-area"
         },
         {
-          "yaw": 2.712320263162596,
-          "pitch": 0.018326044221440796,
+          "yaw": 2.7020203317258664,
+          "pitch": 0.013394073897076808,
           "rotation": 0,
-          "target": "1-vendor-area"
+          "target": "2-vendor-area"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-seating-area",
+      "name": "Seating Area",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3200,
+      "initialViewParameters": {
+        "yaw": 2.9361317417067054,
+        "pitch": -0.015638459005696603,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0504079682053273,
+          "pitch": -0.07835010590752489,
+          "rotation": 0,
+          "target": "2-vendor-area"
+        },
+        {
+          "yaw": 3.0553202411192837,
+          "pitch": -0.07707156244661384,
+          "rotation": 0,
+          "target": "0-bridge-to-concourse"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-vendor-area",
+      "name": "Vendor Area",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3200,
+      "initialViewParameters": {
+        "yaw": -2.5748795822162265,
+        "pitch": 0.1424837376075061,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5653532796246274,
+          "pitch": 0.015758667035058238,
+          "rotation": 0,
+          "target": "0-bridge-to-concourse"
+        },
+        {
+          "yaw": 3.135258261247717,
+          "pitch": 0.07334999647829932,
+          "rotation": 0,
+          "target": "1-seating-area"
         }
       ],
       "infoHotspots": []
@@ -148,8 +148,8 @@ var APP_DATA = {
   "name": "Project Title",
   "settings": {
     "mouseViewMode": "drag",
-    "autorotateEnabled": false,
-    "fullscreenButton": false,
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
     "viewControlButtons": false
   }
 };
